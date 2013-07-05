40b527819b0b220027133790ca4c8e1af4de2640
Andrew Scott <andrew@aes.id.au>
2013-07-05 13:05:13 +0000

    Removed original text

--- web.js
+++ web.js
@@ -7,8 +7,6 @@ app.get('/', function(request, response) {
   fs.readFile('index.html', function (err, data) {
     response.send(data.toString());
   });
-  
-  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
